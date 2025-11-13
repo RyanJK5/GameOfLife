c654a476c77431612529db92cde1463d77751e77
Ryan <ryan.keane051@gmail.com>
2025-11-13 13:35:43 -0500

    [REFACTOR] updated main function to catch all exceptions by reference

--- GameOfLife/src/Application.cpp
+++ GameOfLife/src/Application.cpp
@@ -10,7 +10,7 @@ int main()
         gol::Game game {};
         game.Begin();
     }
-    catch (std::exception e)
+    catch (const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
         return -1;
